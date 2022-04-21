407c305328a8f9182246be2f5d625c65853fe42c
Suraj Geddam <suraj.geddam@gmail.com>
2022-04-20 23:24:30 -0400

    update evaluate and train scripts

--- evaluate.sh
+++ evaluate.sh
@@ -4,9 +4,17 @@ python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_d
 python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_nfsp_result/model.pth' 'random'
 # CFR vs random
 python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_cfr_result/cfr_model' 'random'
+# PPO vs random
+python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_ppo_result/model.pth' 'random'
 # DQN vs NFSP
 python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_dqn_result/model.pth' 'experiments/leduc_holdem_nfsp_result/model.pth'
 # DQN vs CFR
 python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_dqn_result/model.pth' 'experiments/leduc_holdem_cfr_result/cfr_model'
 # NFSP vs CFR
 python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_nfsp_result/model.pth' 'experiments/leduc_holdem_cfr_result/cfr_model'
+# DQN vs PPO
+python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_dqn_result/model.pth' 'experiments/leduc_holdem_ppo_result/model.pth'
+# NFSP vs PPO
+python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_nfsp_result/model.pth' 'experiments/leduc_holdem_ppo_result/model.pth'
+# CFR vs PPO
+python rlcard/examples/evaluate.py --cuda 0 --models 'experiments/leduc_holdem_cfr_result/cfr_model' 'experiments/leduc_holdem_ppo_result/model.pth'
--- train.sh
+++ train.sh
@@ -4,3 +4,5 @@ python rlcard/examples/run_cfr.py
 python rlcard/examples/run_rl.py --cuda 0
 # train nfsp on leduc holdem
 python rlcard/examples/run_rl.py --cuda 0 --algorithm nfsp --log_dir 'experiments/leduc_holdem_nfsp_result/'
+# train ppo on leduc holdem
+python rlcard/examples/run_ppo.py --cuda 0
